9d48efb4b9cf2d54a705a7684125e42867ce4c3f
KirstenCULV <149455238+KirstenCULV@users.noreply.github.com>
2025-10-23 03:03:08 +0000

    update 2

--- Quiz1.cs
+++ Quiz1.cs
@@ -13,7 +13,7 @@ namespace Quiz1
         {
 
 
-            // user input
+            // menu display
             Console.WriteLine("*****Welcome to Tacos La Villa POS System***** \n");
             Console.WriteLine("Menu:");
             Console.WriteLine("A - 3 Tacos Combo - $7.00 / $9.00 with drink");
@@ -21,7 +21,30 @@ namespace Quiz1
             Console.WriteLine("C - Quesadilla Special - $6.00 / $8.00 with drink");
             Console.WriteLine("D - Loaded Nachos - $5.75 / $7.75 with drink");
 
-          
+            // user input
+            Console.WriteLine("Please enter your name: ");
+            Console.WriteLine("Please enter your package code: ");
+            Console.WriteLine("Would you like to add a drink?(Y/N): ");
+            Console.WriteLine("How many combos would you like?: ");
+
+            // process input
+
+
+            // output
+            Console.WriteLine("=======Tacos La Villa Receipt=======");
+            Console.WriteLine("Customer Name: ");
+            Console.WriteLine("Combo Ordered: ");
+            Console.WriteLine("Drink Included: ");
+            Console.WriteLine("Quantity: ");
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Price Per Item: ");
+            Console.WriteLine("Subtotal: ");
+            Console.WriteLine("Tax(9.5%): ");
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Total: ");
+            Console.WriteLine("Free Dessert?");
+            Console.WriteLine("===================");
+            Console.WriteLine("Thank you for supporting Tacos La Villa!");
 
         }
     }
