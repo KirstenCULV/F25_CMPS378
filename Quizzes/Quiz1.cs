23d0f39cfa4661e385e1353ca591c433ff6d81cf
KirstenCULV <149455238+KirstenCULV@users.noreply.github.com>
2025-10-24 16:27:07 -0700

    Update Quiz1.cs

--- Quizzes/Quiz1.cs
+++ Quizzes/Quiz1.cs
@@ -12,82 +12,95 @@ namespace Quiz1
         {
             // intialize variables
             string name, comboOrdered, drinkIncluded, freeDessert = " ";
-            char packageCode, drink = ' ';
-            int comboNum = 0;
+            char drink, packageCode = ' ';
             double taxRate = 9.5D;
-            double itemPrice, perItem, subtotal, dessertDifference, taxAmount, total = 0D;
+            double itemPrice, comboNum, perItem, subtotal, dessertDifference, taxAmount, total = 0D;
 
             // menu display
-            Console.WriteLine("*****Welcome to Tacos La Villa POS System***** \n");
+            Console.WriteLine("********Welcome to Tacos La Villa POS System******** \n");
             Console.WriteLine("Menu:");
-            Console.WriteLine("A - 3 Tacos Combo - $7.00 / $9.00 with drink");
-            Console.WriteLine("B - Burrito Meal - $8.50 / $10.50 with drink");
+            Console.WriteLine("A - 3 Tacos Combo - $7.00      / $9.00 with drink");
+            Console.WriteLine("B - Burrito Meal - $8.50       / $10.50 with drink");
             Console.WriteLine("C - Quesadilla Special - $6.00 / $8.00 with drink");
-            Console.WriteLine("D - Loaded Nachos - $5.75 / $7.75 with drink");
+            Console.WriteLine("D - Loaded Nachos - $5.75      / $7.75 with drink \n");
 
             // user input
             Console.WriteLine("Please enter your name: ");
-            name = Console.ReadLine;
+            name = Console.ReadLine();
             Console.WriteLine("Please enter your package code: ");
-            packageCode = Convert.ToChar(Console.ReadLine);
+            packageCode = Convert.ToChar(Console.ReadLine());
             Console.WriteLine("Would you like to add a drink?(Y/N): ");
-            drink = Convert.ToChar(Console.ReadLine);
-            Console.WriteLine("How many combos would you like?: ");
-            comboNum = Convert.ToInt32(Console.ReadLine);
+            drink = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("How many combos would you like?: \n");
+            comboNum = Convert.ToDouble(Console.ReadLine());
 
             // process input
-            if (drink = 'Y' || 'y')
+            if (drink == 'Y' || drink == 'y')
             {
-                itemPrice = 2.00;
+                itemPrice = 2.00D;
+				perItem = 2.00D;
                 drinkIncluded = "Yes";
             }
 
             else
             {
                 drinkIncluded = "No";
+				itemPrice = 0.00D;
+				perItem = 0.00D;
             }
 
-            if (comboNum = 'A' || 'a')
+            if (packageCode == 'A' || packageCode == 'a')
             {
                 comboOrdered = "3 Tacos Combo";
-                perItem = 7.00;
+                perItem = (perItem + 7.00D);
                 itemPrice = (itemPrice + perItem);
+				double roundedValue = Math.Round(itemPrice, 2);
             }
 
-            if (comboNum = 'B' || 'b')
+            if (packageCode == 'B' || packageCode == 'b')
             {
                 comboOrdered = "Burrito Meal";
-                perItem = 8.50;
+                perItem = (perItem + 8.50D);
                 itemPrice = (itemPrice + perItem);
             }
 
-            if (comboNum = 'C' || 'c')
+            if (packageCode == 'C' || packageCode == 'c')
             {
                 comboOrdered = "Quesadilla Special";
-                perItem = 6.00;
+                perItem = (perItem + 6.00D);
                 itemPrice = (itemPrice + perItem);
             }
 
-            if (comboNum = 'D' || 'd')
+            if (packageCode == 'D' || packageCode == 'd')
             {
                 comboOrdered = "Loaded Nachos";
-                perItem = 5.75;
+                perItem = (perItem + 5.75D);
                 itemPrice = (itemPrice + perItem);
             }
+			
+			else
+			{
+				comboOrdered = "Invalid Selection";
+				perItem = 0.00D;
+			}
 
             subtotal = (itemPrice * comboNum);
-            taxAmount = ((subtotal * 9.5) / 100);
+			double roundedValue = Math.Round(subtotal, 2);
+			
+            taxAmount = ((subtotal * taxRate) / 100D);
+			double roundedvalue = Math.Round(taxAmount, 2);
+			
             total = (taxAmount + subtotal);
 
-            if (subtotal > 20.00)
+            if (subtotal > 20.00D)
             {
                 freeDessert = "Congratulaions! You get a free dessert!";
             }
             
             else
             {
-                dessertDifference = (20.00 - subtotal);
-                freeDessert = "Only " + dessertDifference + " more to earn a free dessert!";
+                dessertDifference = (20.00D - subtotal);
+                freeDessert = "Only $" + dessertDifference + " more to earn a free dessert!";
             }
 
             // output
@@ -109,5 +122,3 @@ namespace Quiz1
         }
     }
 }
-
-
